252515ce1c42c16ac85caf3cb221071cfd89b39e
Rahul <56730814+rahultanwar8814@users.noreply.github.com>
2025-11-04 09:38:43 +0000

    Install Python Linux

--- main.py
+++ main.py
@@ -1,6 +1,1 @@
-import json
-with open('Python//data.json', 'r') as file:
-    json_data = file.read()
-data = json.loads(json_data)
-designations_with_null_url = [item['DESIGNATION'] for item in data['result']['list'] if item['URL'] is None]
-print(designations_with_null_url)
+print("Hello from Python!")
---
+++ python/notes/notes.md
@@ -0,0 +1,16 @@
+Install Python Linux (or in GitHub Codespaces):
+
+Run these commands in your terminal:
+
+Step-1 :-
+sudo apt-get update
+sudo apt-get install python3 -y
+
+Step-2 :-
+python3 --version
+
+Step-3 :-
+echo 'print("Hello from Python!")' > main.py
+
+Step-4 :-
+python3 main.py
