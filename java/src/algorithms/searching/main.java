e4cb5fd13c30ca763ac17f42bf701c17fa6fad09
rahultanwardev <rahul.tanwar.dev@gmail.com>
2026-04-09 18:41:08 +0530

    binary search

--- java/src/algorithms/searching/main.java
+++ java/src/algorithms/searching/main.java
@@ -1,39 +1,22 @@
 public class main {
-
-    public static int binarySearch(int[] arr, int target) {
-
-        int left = 0;
-        int right = arr.length - 1;
-
-        while (left <= right) {
-
-            int mid = left + (right - left) / 2;
-
-            if (arr[mid] == target) {
-                return mid; // element found
-            }
-
-            if (arr[mid] < target) {
-                left = mid + 1;   // search right half
-            } else {
-                right = mid - 1;  // search left half
-            }
-        }
-
-        return -1; // element not found
+  public static int binarySearch(int[] arr,int target){
+    int left = 0;
+    int right = arr.length-1;
+    while(left <= right){
+      int mid = left + (right-left) /2 ;
+      if(target ==  arr[mid]){
+        return mid;
+      }
+      if(arr[mid] < target){
+         left = mid + 1;
+      }else{
+        right = mid- 1;
+      }
     }
-
-    public static void main(String[] args) {
-
-        int[] arr = {2, 4, 6, 8, 10, 12, 14, 16};
-        int target = 10;
-
-        int result = binarySearch(arr, target);
-
-        if (result != -1) {
-            System.out.println("Element found at index: " + result);
-        } else {
-            System.out.println("Element not found");
-        }
-    }
+    return -1;
+  }
+  public static void main(String[] args) {
+    int[] arr = {2,4,6,8,10,12};
+    System.out.println(binarySearch(arr,1));
+  }
 }
--- main.java
+++ main.java
@@ -1,5 +1,22 @@
 public class main {
+  public static int binarySearch(int[] arr,int target){
+    int left = 0;
+    int right = arr.length-1;
+    while(left <= right){
+      int mid = left + (right-left) /2 ;
+      if(target ==  arr[mid]){
+        return mid;
+      }
+      if(arr[mid] < target){
+         left = mid + 1;
+      }else{
+        right = mid- 1;
+      }
+    }
+    return -1;
+  }
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    int[] arr = {2,4,6,8,10,12};
+    System.out.println(binarySearch(arr,1));
   }
 }
