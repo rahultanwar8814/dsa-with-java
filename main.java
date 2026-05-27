de2ab758a7fb13c91a0ad8c552ee3b89b387936a
rahultanwardev <rahul.tanwar.dev@gmail.com>
2026-05-27 16:37:48 +0530

    mn

--- main.java
+++ main.java
@@ -2,54 +2,25 @@ public class main {
 
     public static void main(String[] args) {
 
-        int[][] maze = {
-                {0, 0, 1, 0, 0},
-                {0, 0, 0, 0, 0},
-                {0, 0, 0, 1, 0},
-                {1, 1, 0, 1, 1},
-                {0, 0, 0, 0, 0}
-        };
+        
+        String[][] board = {
+        {".",".",".","."},
+        {".",".",".","."},
+        {".",".",".","."},
+        {".",".",".","."}
+    };
 
-        findPth(maze, 0, 0, "");
+
+        for (int i = 0; i < board.length; i++) {
+
+            for (int j = 0; j < board[0].length; j++) {
+
+                System.out.print(board[i][j]);
+
+            }
+            System.out.print("\n");
+
+        }
     }
 
-    public static void findPth(
-            int[][] maze,
-            int row,
-            int column,
-            String path
-    ) {
-
-        if (row < 0 || column < 0 ||
-                row >= maze.length ||
-                column >= maze[0].length ||
-                maze[row][column] == 1) {
-
-            return;
-        }
-
-        // destination
-        if (row == 4 && column == 4) {
-            System.out.println(path);
-            return;
-        }
-
-        // visited
-        maze[row][column] = 1;
-
-        // Left
-        findPth(maze, row, column - 1, path + "L");
-
-        // Right
-        findPth(maze, row, column + 1, path + "R");
-
-        // Up
-        findPth(maze, row - 1, column, path + "U");
-
-        // Down
-        findPth(maze, row + 1, column, path + "D");
-
-        // backtrack
-        maze[row][column] = 0;
-    }
 }
